56c5ba3c725a3d5778c7d6adab4c4c586d9ceb18
wildpenguin <wildpenguin@gmail.com>
2015-12-29 17:38:59 -0500

    add example code

---
+++ examples/backend.php
@@ -0,0 +1,29 @@
+<?php 
+/**
+* The front end of the praiseboard stats
+* this code usually goes into Controller and View
+* I dont want to stick to any framework for the moment
+* just explaining the idea
+* author: wildpenguin@gmail.com
+* 
+*/
+ 	require_once('PraiseBoard.php');
+
+		// these data comes from the db
+		$data = array(
+			'bob'=>141,
+			'todd'=>190,
+			'mark'=>50,
+			'mary'=>301,
+			'katy'=>150,
+		);
+
+		$praiseBoard = new PraiseBoard($data);
+		$stats = $praiseBoard->buildStats();
+
+		echo json_encode( array(
+			'stats'=>$stats,
+		));
+
+	?>
+
